e996f004e3f5aac6ae919cb45716bc953c217f32
Marek Rams <marek.rams@uj.edu.pl>
2021-09-16 09:50:07 +0200

    rename aux.py to auxx.py to make it windows-friendly

--- tnac4o/__init__.py
+++ tnac4o/__init__.py
@@ -1,2 +1,2 @@
 from .tnac4o import tnac4o, load
-from .aux import load_Jij, round_Jij, minus_Jij, Jij_f2p, energy_Jij, energy_RMF
+from .auxx import load_Jij, round_Jij, minus_Jij, Jij_f2p, energy_Jij, energy_RMF
